5638f9adf8f94aaf4f80b1a9143463c628f14292
JAlvarezz18 <80436651+JAlvarezz18@users.noreply.github.com>
2022-05-24 07:49:17 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typing', {
-    strings:['','GRACIAS ("ES EL FIN")','THANK YOU ("IT IS THE END")','MERCI ("CEST LA FIN")','СПАСИБО ("ЭТО КОНЕЦ")','OBRIGADO ("ISTO É O FIM")','ΕΥΧΑΡΙΣΤΏ ("ΑΥΤΌ ΕΊΝΑΙ ΤΟ ΤΈΛΟΣ")','GRAZIE ("QUESTA È LA FINE")','おつかれさまでした','ĎAKUJEM ("TOTO JE KONIEC")','QUE LE IMPORTA SAPO PERRO HP XD',],
+    strings:['','GRACIAS ("ES EL FIN")','THANK YOU ("IT IS THE END")','MERCI ("CEST LA FIN")','СПАСИБО ("ЭТО КОНЕЦ")','OBRIGADO ("ISTO É O FIM")','ΕΥΧΑΡΙΣΤΏ ("ΑΥΤΌ ΕΊΝΑΙ ΤΟ ΤΈΛΟΣ")','GRAZIE ("QUESTA È LA FINE")','おつかれさまでした','ĎAKUJEM ("TOTO JE KONIEC")'],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
